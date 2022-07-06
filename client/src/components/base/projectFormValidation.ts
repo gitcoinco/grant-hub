f29b51ae8ab3825e6d6b7919267af75bb67d1c84
schultztimothy <schultz.timothy52@gmail.com>
2022-07-06 16:09:37 -0600

    Updated error messages

--- client/src/components/base/projectFormValidation.ts
+++ client/src/components/base/projectFormValidation.ts
@@ -10,11 +10,13 @@ export interface FormSchema {
 
 export async function validateProjectForm(inputs: FormSchema) {
   const schema = object({
-    title: string().required(),
-    description: string().required(),
-    website: string().url().required(),
-    challenges: string().required(),
-    roadmap: string().required(),
+    title: string().required("Project Name is required"),
+    description: string().required("Project Description is required"),
+    website: string()
+      .url("Project Website must be a valid url. e.g. https://gitcoin.co/")
+      .required("Project Website is required"),
+    challenges: string().required("Project Challenges is required"),
+    roadmap: string().required("Project Roadmap is required"),
   });
 
   const validatedInputs = await schema.validate(inputs);
