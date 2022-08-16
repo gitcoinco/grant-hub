1158754283d430cbafb361535a06500e1fce292a
Jaxcoder <codenamejason@outlook.com>
2022-08-16 13:29:20 -0400

    add env for what chains to use per environment

--- client/src/utils/wagmi.ts
+++ client/src/utils/wagmi.ts
@@ -1,21 +1,25 @@
-import { chain, configureChains, createClient } from "wagmi";
+import { getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { Chain, chain, configureChains, createClient } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { infuraProvider } from "wagmi/providers/infura";
 import { publicProvider } from "wagmi/providers/public";
-import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 
 // RPC keys
 const alchemyId = process.env.ALCHEMY_ID;
 const infuraId = process.env.INFURA_ID;
 
-export const { chains, provider } = configureChains(
-  [chain.goerli, chain.optimismKovan, chain.optimism],
-  [
-    infuraProvider({ apiKey: infuraId, priority: 0 }),
-    alchemyProvider({ apiKey: alchemyId, priority: 1 }),
-    publicProvider({ priority: 2 }),
-  ]
-);
+const chainsAvailable: Chain[] = [];
+if (process.env.NODE_ENV === "production") {
+  chainsAvailable.push(chain.optimism);
+} else {
+  chainsAvailable.push(chain.goerli, chain.optimismKovan);
+}
+
+export const { chains, provider } = configureChains(chainsAvailable, [
+  infuraProvider({ apiKey: infuraId, priority: 0 }),
+  alchemyProvider({ apiKey: alchemyId, priority: 1 }),
+  publicProvider({ priority: 2 }),
+]);
 
 const { connectors } = getDefaultWallets({
   appName: "Grant Hub",
