03aef237b4f8dbf89cfac9ea77e0996001eceb55
Kurt <Kurt@Merbeth.io>
2023-01-06 12:12:12 +0100

    shortened code

--- client/src/history.ts
+++ client/src/history.ts
@@ -1,7 +1,4 @@
 import { createHashHistory, createBrowserHistory } from "history";
 import { isIpfsBasename } from "./utils/ipfs";
 
-const hashHistory = createHashHistory();
-const browserHistory = createBrowserHistory();
-
-export default isIpfsBasename() ? hashHistory : browserHistory;
+export default isIpfsBasename() ? createHashHistory() : createBrowserHistory();
