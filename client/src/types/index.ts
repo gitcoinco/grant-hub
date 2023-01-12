6c949529c9bf00fe1e8e0f8cea7dd81eab99d6ad
Mohamed Boudra <boudra.moha@gmail.com>
2023-01-12 15:02:31 +0100

    allow configuring rows and the container class for textareas

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -61,11 +61,14 @@ export type InputProps = {
   changeHandler: (event: ChangeHandlers) => void;
   required: boolean;
   encrypted?: boolean;
+  containerClass?: string;
   feedback: {
     type: string;
     message: string;
   };
 };
+
+export type TextAreaProps = InputProps & { rows?: number };
 
 export type AddressInputProps = {
   label: string;
