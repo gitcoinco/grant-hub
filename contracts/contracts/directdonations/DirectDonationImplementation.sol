0164791008190714f499b21fc5b3fbbc49cfb652
KurtMerbeth <Kurt@Merbeth.io>
2023-01-25 12:14:36 +0100

    DirectDonationImplementation and IVotingStrategy

---
+++ contracts/contracts/directdonations/DirectDonationImplementation.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+
+import "./IVotingStrategy.sol";
+
+/**
+ * Allows voters to cast multiple weighted votes to grants with one transaction
+ * This is inspired from BulkCheckout documented over at:
+ * https://github.com/gitcoinco/BulkTransactions/blob/master/contracts/BulkCheckout.sol
+ *
+ * Emits event upon every transfer.
+ */
+contract DirectDonationImplementation is IVotingStrategy, Initializable, ReentrancyGuardUpgradeable {
+
+  using SafeERC20Upgradeable for IERC20Upgradeable;
+
+  string public constant VERSION = "0.2.0";
+
+  // --- Event ---
+
+  /// @notice Emitted when a new vote is sent
+  event Voted(
+    address token,                    // voting token
+    uint256 amount,                   // voting amount
+    address indexed voter,            // voter address
+    address grantAddress,             // grant address
+    bytes32 indexed projectId,        // project id
+    address indexed roundAddress      // round address
+  );
+
+  // --- Core methods ---
+
+  function initialize() external initializer {
+    // empty initializer
+  }
+
+  /**
+ *
+   * @dev
+   * - supports ERC20 and Native token transfer
+   *
+   * @param encodedVote encoded vote
+   * @param voterAddress voter address
+   */
+  function _vote(bytes calldata encodedVote, address voterAddress) internal {
+ /// @dev decode encoded vote
+      (
+        address _token,
+        uint256 _amount,
+        address _grantAddress,
+        bytes32 _projectId
+      ) = abi.decode(encodedVote, (
+        address,
+        uint256,
+        address,
+        bytes32
+      ));
+
+      if (_token == address(0)) {
+        /// @dev native token transfer to grant address
+        // slither-disable-next-line reentrancy-events
+        AddressUpgradeable.sendValue(payable(_grantAddress), _amount);
+      } else {
+
+        /// @dev erc20 transfer to grant address
+        // slither-disable-next-line arbitrary-send-erc20,reentrancy-events,
+        SafeERC20Upgradeable.safeTransferFrom(
+          IERC20Upgradeable(_token),
+          voterAddress,
+          _grantAddress,
+          _amount
+        );
+
+      }
+
+      /// @dev emit event for transfer
+      emit Voted(
+        _token,
+        _amount,
+        voterAddress,
+        _grantAddress,
+        _projectId,
+        msg.sender
+      );
+  }
+
+  function vote(bytes[] calldata encodedVotes, address voterAddress) external override payable nonReentrant {
+    /// @dev iterate over multiple donations and transfer funds
+    for (uint256 i = 0; i < encodedVotes.length; i++) {
+      _vote(encodedVotes[i], voterAddress);
+    }
+  }
+
+    function vote(bytes calldata encodedVote, address voterAddress) external payable nonReentrant {
+      _vote(encodedVote, voterAddress);
+  }
+}
