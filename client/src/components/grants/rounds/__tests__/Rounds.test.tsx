6a75675ab0f702c2475469e81233a4f56e2f1e77
Jaxcoder <codenamejason@outlook.com>
2023-01-26 15:29:03 -0500

    lint

--- client/src/components/grants/rounds/__tests__/Rounds.test.tsx
+++ client/src/components/grants/rounds/__tests__/Rounds.test.tsx
@@ -6,44 +6,46 @@ import {
   addressFrom,
   buildProjectApplication,
   buildRound,
-  renderWrapped
+  renderWrapped,
 } from "../../../../utils/test_utils";
 import Rounds from "../Rounds";
- 
+
 // the params are needed to create the unique project id, etc..
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
   useParams: () => ({
     chainId: "5",
     id: "2",
-    registryAddress: addressFrom(123)
+    registryAddress: addressFrom(123),
   }),
 }));
- 
+
 describe("<Rounds />", () => {
   afterEach(() => {
     cleanup();
   });
- 
+
   describe("When the data is loading", () => {
     test("should show loading", async () => {
       await act(async () => {
         renderWrapped(<Rounds />, setupStore());
       });
- 
+
       expect(
         screen.getByText("Loading your information, please stand by...")
       ).toBeInTheDocument();
     });
   });
- 
+
   describe("when the data is loaded", () => {
     const store = setupStore();
     store.dispatch(web3ChainIDLoaded(5));
- 
+
     test.only("should show the active rounds", async () => {
       const round1 = buildRound({
-        address: addressFrom(1), applicationsEndTime: 1663751954, // you need to set the correct application and round times which fits into your if statements
+        address: addressFrom(1),
+        applicationsEndTime: 1663751954,
+        // you need to set the correct application and round times which fits into your if statements
       });
       const round2 = buildRound({
         address: addressFrom(2),
@@ -60,7 +62,8 @@ describe("<Rounds />", () => {
       });
       const applications = [];
       applications.push(
-        buildProjectApplication({ roundID: addressFrom(1), status: "APPROVED" }) // set the status directly here, saves some pain
+        buildProjectApplication({ roundID: addressFrom(1), status: "APPROVED" })
+        // set the status directly here, saves some pain
       );
       applications.push(
         buildProjectApplication({ roundID: addressFrom(2), status: "REJECTED" })
@@ -70,13 +73,13 @@ describe("<Rounds />", () => {
         applications,
         projectID: "2",
       });
- 
+
       await act(async () => {
         renderWrapped(<Rounds />, store);
       });
       expect(screen.getByText("Active Rounds")).toBeInTheDocument();
     });
- 
+
     test("should show the current applications", async () => {
       await act(async () => {
         renderWrapped(<Rounds />, store);
