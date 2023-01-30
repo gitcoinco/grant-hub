f814b938a3c7de55e01616924311c98f59c097f9
KurtMerbeth <Kurt@Merbeth.io>
2023-01-30 10:40:28 +0100

    removed routes logging

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,5 +1,3 @@
-import { datadogLogs } from "@datadog/browser-logs";
-
 export const slugs = {
   root: `/`,
   grants: `/projects`,
@@ -10,58 +8,27 @@ export const slugs = {
   roundApplication: `/chains/:chainId/rounds/:roundId/apply`,
 };
 
-export const rootPath = () => {
-  datadogLogs.logger.info(`====> Route: ${slugs.root}`);
-  datadogLogs.logger.info(`====> URL: ${window.location.href}`);
-  return slugs.root;
-};
+export const rootPath = () => slugs.root;
 
-export const grantsPath = () => {
-  datadogLogs.logger.info(`====> Route: ${slugs.grants}`);
-  datadogLogs.logger.info(`====> URL: ${window.location.href}`);
-  return slugs.grants;
-};
+export const grantsPath = () => slugs.grants;
 
-export const newGrantPath = () => {
-  datadogLogs.logger.info(`====> Route: ${slugs.newGrant}`);
-  datadogLogs.logger.info(`====> URL: ${window.location.href}`);
-  return slugs.newGrant;
-};
+export const newGrantPath = () => slugs.newGrant;
 
 export const projectPath = (
   chainId: string,
   registryAddress: string,
   projectId: string
-) => {
-  datadogLogs.logger.info(
-    `====> Route: /chains/${chainId}/registry/${registryAddress}/projects/${projectId}`
-  );
-  datadogLogs.logger.info(`====> URL: ${window.location.href}`);
-  return `/chains/${chainId}/registry/${registryAddress}/projects/${projectId}`;
-};
+) => `/chains/${chainId}/registry/${registryAddress}/projects/${projectId}`;
 
 export const editPath = (
   chainId: string,
   registryAddress: string,
   projectId: string
-) => {
-  datadogLogs.logger.info(
-    `====> Route: /chains/${chainId}/registry/${registryAddress}/projects/${projectId}/edit`
-  );
-  datadogLogs.logger.info(`====> URL: ${window.location.href}`);
-  return `/chains/${chainId}/registry/${registryAddress}/projects/${projectId}/edit`;
-};
+) =>
+  `/chains/${chainId}/registry/${registryAddress}/projects/${projectId}/edit`;
 
-export const roundPath = (chainId: string, roundId: string) => {
-  datadogLogs.logger.info(`====> Route: /chains/${chainId}/rounds/${roundId}`);
-  datadogLogs.logger.info(`====> URL: ${window.location.href}`);
-  return `/chains/${chainId}/rounds/${roundId}`;
-};
+export const roundPath = (chainId: string, roundId: string) =>
+  `/chains/${chainId}/rounds/${roundId}`;
 
-export const roundApplicationPath = (chainId: string, roundId: string) => {
-  datadogLogs.logger.info(
-    `====> Route: /chains/${chainId}/rounds/${roundId}/apply`
-  );
-  datadogLogs.logger.info(`====> URL: ${window.location.href}`);
-  return `/chains/${chainId}/rounds/${roundId}/apply`;
-};
+export const roundApplicationPath = (chainId: string, roundId: string) =>
+  `/chains/${chainId}/rounds/${roundId}/apply`;
